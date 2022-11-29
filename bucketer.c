8d3c27ceb8da9524255841d84f401215ee32f316
AshleshaPai <105655752+AshleshaPai@users.noreply.github.com>
2022-11-29 16:29:46 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -9,19 +9,22 @@ struct CountsByUsage {
 
 struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
   struct CountsByUsage counts = {0, 0, 0};
-  
-  // loop begins from i=0 -> nBatteries-1
-
-  for(int i=0;i<nBatteries;++i)
-  {
-      if(cycles[i]<410)counts.lowCount+=1;  // checks the status of Battery if it is less than 410 then Low is printed
-      else if(cycles[i]>=410 && cycles[i]<=949)counts.mediumCount+=1;  // else if it is greater than 410  and less 949 than then Medium is printed
-      else counts.highCount+=1; // else if it is greater than 950 than then High is printed
+  int i,m,l,h;
+  m=l=h=0;
+  for(i=0;i<nBatteries;i++){
+    if(cycles[i]<=410){
+      (counts.lowCount)++;
+    }
+    else if(cycles[i]>410 && cycles[i]<950){
+      (counts.mediumCount)++;
+    }
+    else{
+      (counts.highCount)++;
+    }
   }
-  
   return counts;
 }
-
+  
 void testBucketingByNumberOfCycles() {
   const int chargeCycleCounts[] = {100, 300, 500, 600, 900, 1000};
   const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
